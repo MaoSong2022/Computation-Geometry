f33941f39fcad569f48dc48193a703ff66117ce5
MaoSong2022 <maosong@pjlab.org.cn>
2023-07-03 14:15:28 +0800

    fix(polygon.cpp): fix index error

--- src/polygon.cpp
+++ src/polygon.cpp
@@ -4,13 +4,13 @@
 
 namespace GJK {
 Point Polygon::support(const Point& d) const {
-  int index = 0;
+  int index = -1;
   double max_value = std::numeric_limits<double>::lowest();
   for (int i = 0; i < vertices_.size(); ++i) {
     double dot_product = (vertices_[i] - center_).dot_product(d);
     if (dot_product > max_value) {
       max_value = dot_product;
-      i = index;
+      index = i;
     }
   }
   return vertices_[index];
