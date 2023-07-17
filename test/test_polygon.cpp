9a59a6ca1f9925ec71f97a736ed5662e2d7b9966
MaoSong2022 <maosong@pjlab.org.cn>
2023-07-17 09:25:17 +0800

    test(test_polygon): add test for contains function

--- test/test_polygon.cpp
+++ test/test_polygon.cpp
@@ -14,7 +14,7 @@ class PolygonTestSuite : public ::testing::Test {
 
   void TearDown() override { delete triangle1; }
 
-  Geometry::Shape *triangle1;
+  Geometry::Polygon *triangle1;
   const double kEPS = 1e-3;
 };
 
@@ -30,4 +30,15 @@ TEST_F(PolygonTestSuite, support) {
   ASSERT_NEAR(d.x, 9.0, kEPS);
   ASSERT_NEAR(d.y, 9.0, kEPS);
   ASSERT_NEAR(d.z, 0.0, kEPS);
+}
+
+TEST_F(PolygonTestSuite, contains) {
+  Geometry::Vec3d point1 = Geometry::Vec3d(4, 8);
+  Geometry::Vec3d point2 = Geometry::Vec3d(8.2, 8);
+  Geometry::Vec3d point3 = Geometry::Vec3d(6, 9);
+  Geometry::Vec3d point4 = Geometry::Vec3d(8.18, 9.18);
+  ASSERT_TRUE(triangle1->contains(point3));
+  ASSERT_TRUE(triangle1->contains(point4));
+  ASSERT_FALSE(triangle1->contains(point1));
+  ASSERT_FALSE(triangle1->contains(point2));
 }
